c7e65405b3748ab2eb694ffaf08e52f952c92362
CS_Club <wkd108go@gmail.com>
2022-09-22 08:42:01 +0900

    IAR EW for Arm 9.20.3 버전으로 컴파일.

--- EWARM/settings/RFM.RFM.cspy.ps1
+++ EWARM/settings/RFM.RFM.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\D\Git_Hub\STM_IAR_SIL_Change\EWARM\settings\RFM.RFM.general.xcl" --backend -f "C:\D\Git_Hub\STM_IAR_SIL_Change\EWARM\settings\RFM.RFM.driver.xcl" 
+& "C:\Program Files\IAR Systems\Embedded Workbench 9.0 EWARM FS 9.20.3\common\bin\cspybat" -f "C:\D\Git_Hub\STM_IAR_SIL_Change\EWARM\settings\RFM.RFM.general.xcl" --backend -f "C:\D\Git_Hub\STM_IAR_SIL_Change\EWARM\settings\RFM.RFM.driver.xcl" 
 }
 else
 {
-& "C:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin\cspybat" -f "C:\D\Git_Hub\STM_IAR_SIL_Change\EWARM\settings\RFM.RFM.general.xcl" --debug_file=$debugfile --backend -f "C:\D\Git_Hub\STM_IAR_SIL_Change\EWARM\settings\RFM.RFM.driver.xcl" 
+& "C:\Program Files\IAR Systems\Embedded Workbench 9.0 EWARM FS 9.20.3\common\bin\cspybat" -f "C:\D\Git_Hub\STM_IAR_SIL_Change\EWARM\settings\RFM.RFM.general.xcl" --debug_file=$debugfile --backend -f "C:\D\Git_Hub\STM_IAR_SIL_Change\EWARM\settings\RFM.RFM.driver.xcl" 
 }
